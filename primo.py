652eb09918449ddbc208851244bff05521f7bf02
frncg1tsalvad0r <132061377+frncg1tsalvad0r@users.noreply.github.com>
2025-03-05 19:56:55 +0100

    committato

---
+++ primo.py
@@ -0,0 +1,14 @@
+import math
+
+
+a = input("Inserisci un numero: ")
+print("Il numero inserito è: ", a)
+
+print("la radice quadrata del numero è ", math.sqrt(a))
+
+print("master")
+print('fine')
+
+print("sono ancuraaaaa")
+
+print("dev")
